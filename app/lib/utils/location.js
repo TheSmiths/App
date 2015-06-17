4af557305086c7884f8c388bcad94f248b377e1b
Matthias Benkort <matthias.benkort@gmail.com>
2015-06-17 17:16:35 +0200

    Fix Geolocation on Android

--- app/lib/utils/location.js
+++ app/lib/utils/location.js
@@ -3,8 +3,14 @@ var log = require('utils/log');
 module.exports = {
     getCurrentLatLng: function getCurrentLatLng (callback) {
         if (Ti.Geolocation.locationServicesEnabled) {
-            Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_BEST;
-            Ti.Geolocation.distanceFilter = 10;
+            
+            if (OS_IOS) {
+                Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_BEST;
+                Ti.Geolocation.distanceFilter = 10;
+            } else if (OS_ANDROID) {
+                Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_HIGH;
+            }
+
             Ti.Geolocation.getCurrentPosition(function(evt) {
                 if (!evt.success || evt.error) {
                     log.error('[utils/location] Unable to determine location!');

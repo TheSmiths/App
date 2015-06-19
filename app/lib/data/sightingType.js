1263b79b6c8bdea7236ac883c6bf1dc762539620
PierreGUI <pierre.vdev@gmail.com>
2015-06-19 14:54:54 +0200

    Add one more sighting type

--- app/lib/data/sightingType.js
+++ app/lib/data/sightingType.js
@@ -9,19 +9,26 @@ var sightingTypeData = module.exports = [
     {
         id: 1,
         icon: '/images/icons/default.png',
-        valueLabel: '2 - 10',
+        valueLabel: '2 - 5',
         captionLabel: 'Items of debris',
         type: 'large'
     },
     {
         id: 2,
         icon: '/images/icons/default.png',
-        valueLabel: '11 - 100',
+        valueLabel: '6 - 10',
         captionLabel: 'Items of debris',
         type: 'large'
     },
     {
         id: 3,
+        icon: '/images/icons/default.png',
+        valueLabel: '11 - 100',
+        captionLabel: 'Items of debris',
+        type: 'large'
+    },
+    {
+        id: 4,
         icon: '/images/icons/default.png',
         valueLabel: '100+',
         captionLabel: 'Items of debris',
